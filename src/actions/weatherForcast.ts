dafbe4fff1f5f90159951b10730892f7cceb01da
2raymoori <2raymoori@gmail.com>
2025-02-23 19:46:43 +0100

    commit for adding a client component to search place

--- src/actions/weatherForcast.ts
+++ src/actions/weatherForcast.ts
@@ -1,9 +1,38 @@
 const dailyForecastData = require("@/Utils/daily.json")
 const hourlyForcastData = require("@/Utils/hourly_forcast.json")
-export const hourlylyForcast = async (placeName:string)=>{
+
+const options = {
+        method: 'GET',
+        headers: {
+                'x-rapidapi-key': '8ecaf107fcmshc9b7613293c1a72p1519c9jsn18e93f8cb42b',
+                'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
+        }
+};
+const baseURL = 'https://ai-weather-by-meteosource.p.rapidapi.com/';
+export const hourlylyForcast = async (cityName:string)=>{
+    /*
+    const url = `${baseURL}hourly?place_id=${cityName}&timezone=auto&language=en&units=auto`;
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        return result.hourly.data;//.splice(0,19);
+    } catch (error) {
+        console.error(error);
+    }
+    */
         return hourlyForcastData.hourly.data
 }
 
-export const dailyForcast = async(placeName:string)=>{
-        return dailyForecastData.daily.data.slice(1,11)
+export const dailyForcast = async(cityName:string)=>{
+    /*
+    const url = `${baseURL}daily?place_id=${cityName}&language=en&units=auto`;
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result.daily.data.slice(0,10)
+    } catch (error) {
+        console.error(error);
+    }*/
+        return dailyForecastData.daily.data.slice(0,10)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,32 +7,83 @@ import DailyDisplay from "@/Components/DailyDisplay";
 import UvCard from "@/Components/UvCArd";
 import Compass from "@/Components/Compass";
 import {FaCalendar, FaSearch, FaSearchLocation} from "react-icons/fa";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {CirclesWithBar, MagnifyingGlass} from "react-loader-spinner";
 export default  function Home() {
     const [dailyForecastData,setDailyForecastData] = useState([]);
-    const [hourlyForcastData,sethHurlyForcastData] = useState([]);
+    const [hourlyForecastData,sethHourlyForecastData] = useState([]);
+    const [placeToSearch,setPlaceToSearch] = useState("");
     const [loading,setLoading] = useState(true)
+
+
     const onLoad = async() =>{
         const dailyForecastData = await  dailyForcast("potsdam");
-        const hourlyForcastData = await hourlylyForcast("potsdam");
+        const hourlyForecastData = await hourlylyForcast("potsdam");
+        setDailyForecastData(dailyForecastData)
+        sethHourlyForecastData(hourlyForecastData)
+        setLoading(false);
+    }
+    const onPlaceSearch = async()=>{
+        setLoading(true);
+        const dailyForecastData = await  dailyForcast(placeToSearch);
+        const hourlyForecastData = await hourlylyForcast(placeToSearch);
+        setDailyForecastData(dailyForecastData)
+        sethHourlyForecastData(hourlyForecastData)
+        setLoading(false);
+    }
+    const onCaptureText = (e:ChangeEvent<HTMLInputElement>)=>{
+            setPlaceToSearch(e.target.value)
+    }
+    useEffect(() => {
+        onLoad()
+    }, []);
+
+    if(loading){
+        return (
+            <div className={"border-2 h-screen flex justify-center items-center"}>
+                <CirclesWithBar
+                    height="100"
+                    width="100"
+                    color="#4fa94d"
+                    outerCircleColor="#4fa94d"
+                    innerCircleColor="#4fa94d"
+                    barColor="#4fa94d"
+                    ariaLabel="circles-with-bar-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                /><MagnifyingGlass
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="magnifying-glass-loading"
+                wrapperStyle={{}}
+                wrapperClass="magnifying-glass-wrapper"
+                glassColor="#c0efff"
+                color="#e15b64"
+            />
+
+            </div>
+        )
     }
   return (
       <main className={" max-w-5xl m-auto grid grid-cols-6 gap-2"}>
+
           <div className={"col-start-1 col-end-7 flex flex-row-reverse"}>
               <div className={"border-2 border-orange-500 rounded-xl  flex items-center"}>
-                  <input placeholder={"Place Search"}
-                         className={"rounded-xl py-2 pl-2 "}/>
-                  <button className={"border-2 py-2" }>
+                  <input value={placeToSearch} placeholder={"Place Search"}
+                         className={"rounded-xl py-2 pl-2 "} onChange={onCaptureText} />
+                  <button onClick={onPlaceSearch} className={"border-2 py-2" }>
                       <FaSearchLocation size={30}/>
                   </button>
               </div>
           </div>
           <section className={"border-2 border-orange-500 col-start-1 col-end-7  bg-orange-500  justify-center"}>
               <div className={"border-white border-2 text-center"}>
-              <p>Potsdam</p>
-                  <p>1<sup>o</sup></p>
-                  <p>Mostly Cloudy</p>
-                  <p>H:3<sup>o</sup> L:-6<sup>o</sup></p>
+              <p>{placeToSearch}</p>
+                  <p className={"text-white font-bold text-2xl italic"}>{hourlyForecastData[0].temperature}<sup>o</sup></p>
+                  <p className={"text-white font-bold text-2xl italic"}>{hourlyForecastData[0].weather.toUpperCase()}</p>
+                  <p className={"text-white font-bold text-2xl italic"}>H:{dailyForecastData[0].temperature_max}<sup>o</sup> L:{dailyForecastData[0].temperature_min}<sup>o</sup></p>
               </div>
           </section>
           <section className={"border-2 border-orange-500 col-start-1 col-end-3"}>
@@ -42,7 +93,7 @@ export default  function Home() {
               <p>HOURLY FORECAST</p>
               <div className={"flex justify-around flex-wrap gap-2"}>
                   {
-                      hourlyForcastData.slice(0,18).map(e=>{
+                      hourlyForecastData.slice(0,18).map(e=>{
                           return(
                               <WeatherCard weather={e.weather} key={e.date} hour={hourExtractor(e.date)} temperature={e.temperature} />
                           )
@@ -55,7 +106,7 @@ export default  function Home() {
                   <h1 className={"justify-center text-center flex items-center gap-2 text-orange-500"}><FaCalendar /> 10-DAY FORECAST</h1>
                   {dailyForecastData.map(e=>{
                       return(
-                          <DailyDisplay weatherDescription = {e.weather} key={e.day} date={e.day} minTemp={e.temperature_min} maxTemp={e.temperature_max} />
+                          <DailyDisplay weatherDescription = {""} key={e.day} date={e.day} minTemp={e.temperature_min} maxTemp={e.temperature_max} />
                       )
                   })}
               </div>
@@ -63,7 +114,7 @@ export default  function Home() {
           <section className={"border-2 border-orange-500 col-start-3 col-end-7"}>
               <div className={"grid grid-cols-3 gap-2"}>
                   <Compass windData={dailyForecastData[0].wind} />
-                  <UvCard description={"Low for the rest of the day."} uvValue={hourlyForcastData[0].uv_index} title={"UV INDEX"} showProgressBar={true} />
+                  <UvCard description={"Low for the rest of the day."} uvValue={hourlyForecastData[0].uv_index} title={"UV INDEX"} showProgressBar={true} />
                   <UvCard description={"The dew point is -5 right now."} uvValue={`${dailyForecastData[0].humidity}%`} title={" HUMIDITY"} />
                   <UvCard uvValue={`${dailyForecastData[0].visibility} KM`} description={"Perfectly clear view"}  title={"VISIBILITY"} />
                   <UvCard uvValue={dailyForecastData[0].feels_like} description={"Wind is making it feel colder"} title={"FEELS LIKE"} />
